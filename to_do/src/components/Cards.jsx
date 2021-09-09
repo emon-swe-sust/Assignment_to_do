d81ccb2ea887a3ebdc222dbb1b64e17bc8152383
emon-swe-sust <mahfuzur03@student.sust.edu>
2021-09-10 00:46:40 +0600

    todo completed

--- to_do/src/components/Cards.jsx
+++ to_do/src/components/Cards.jsx
@@ -1,80 +1,153 @@
-import React, {useEffect} from 'react'
+import React, {useState} from "react";
 
-function Cards ({ id, name, email, todo, todoDelete }) {
+function Cards({ id, name, email, todo, todoUpdate, todoDelete }) {
+  const [update, setUpdate] = useState(false)
+  const [info, setInfo] = useState({
+    id: id,
+    name: name,
+    email: email,
+    todo: todo,
+  })
 
-    // function handleDelete(id){   
-    //     console.log(id) 
-    //     let newtodos = todos;
-    //     newtodos.splice(id,1);
-    //     console.log(newtodos)
-    //     setTodos([{'todos':2},])
-    // }
+  function doUpdate(id) {
+    setUpdate(!update);
+    todoUpdate(id, info);
+  }
+
+  function handleChange(e){
+    setInfo({...info, [e.target.name]:e.target.value})
+  }
 
   return (
     <div className='bg-white mt-5 shadow-md lg:w-5/12 mx-6 rounded-lg'>
-      <div className='p-2'>
-        {/* bg-white mt-5 shadow-md hover:bg-blue-100 
-        w-5/12 mx-6 rounded-lg */}
+      <div className='p-5'>
         <label
           className='block text-blue-800 text-md font-semibold'
-          htmlFor='username'
         >
-          Username:
-        </label>
-        <div>{name}</div>
-        <label
-          className='block text-blue-800 text-md font-semibold'
-          htmlFor='username'
-        >
-          Email:
-        </label>
-        <div>{email}</div>
-        <label
-          className='block text-blue-800 text-md font-semibold mt-5'
-          htmlFor='username'
-        >
-          To-Do:
-        </label>
-        <div>{todo}</div>
-        <div className='pt-5 flex mb-2 mx-5'>
-          <button className='ml-auto' onClick={()=>todoDelete(id)}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-8 w-8 text-red-500 shadow-xl
-              hover:-translate-y-1 transform transition hover:text-white
-              hover:bg-red-500'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-            >
-              <path
-                fillRule='evenodd'
-                d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
-                clipRule='evenodd'
-              />
+          <div className='flex'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
             </svg>
-          </button>
-          <button>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-8 w-8 text-purple-500 shadow-xl
+            &nbsp; Username:
+          </div>
+        </label>
+        <div>
+          {update ? (
+            <input
+              className='shadow-xl appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
+              name='name'
+              type='text'
+              placeholder={name}
+              onChange={e => handleChange(e)}
+              required
+            />
+          ) : (
+            name
+          )}
+        </div>
+        <label
+          className='block text-blue-800 text-md font-semibold'
+        >
+          <div className='flex mt-2'>
+
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+            &nbsp; Email:
+          </div>
+
+        </label>
+        <div>
+          {update ? (
+            <input
+              className='shadow-xl appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
+              name='email'
+              type='text'
+              placeholder={email}
+              onChange={e => handleChange(e)}
+              required
+            />
+          ) : (
+            email
+          )}
+        </div>
+        <label
+          className='block text-blue-800 text-md font-semibold mt-10'
+        >
+          <div className='flex'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
+              <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+            </svg>
+            &nbsp; To-Do:
+          </div>
+        </label>
+        <div>
+          {update ? (
+            <textarea
+              className='w-full shadow-xl h-48 md:h-24 px-3 py-2 text-base text-gray-700 border rounded-xl focus:shadow-outline'
+              placeholder={todo}
+              name='todo'
+              onChange={e => handleChange(e)}
+              required
+            />
+          ) : (
+            todo
+          )}
+        </div>
+        <div className='pt-5 mb-2 mx-5'>
+          {
+            update ? (
+              <button className='py-2 px-8 bg-blue-800 text-white
+              flex m-auto rounded-lg hover:-translate-y-1 transform transition
+              hover:bg-blue-500'
+              onClick={() => doUpdate(id)}>
+                Update
+              </button>
+            ) : (
+              <div className='flex ml-auto'>
+                <button className='ml-auto' onClick={() => setUpdate(!update)}>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-8 w-8 text-purple-500 shadow-xl
               hover:-translate-y-1 transform transition hover:text-white
               hover:bg-purple-500'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
-              />
-            </svg>
-          </button>
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth={2}
+                      d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
+                    />
+                  </svg>
+                </button>
+                <button onClick={() => todoDelete(id)}>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-8 w-8 text-red-500 shadow-xl
+              hover:-translate-y-1 transform transition hover:text-white
+              hover:bg-red-500'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                </button>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
   )
 }
 
-export default Cards
+export default Cards;
--- to_do/src/components/Home.jsx
+++ to_do/src/components/Home.jsx
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import Cards from './Cards'
 
-function Home () {
+function Home() {
   const [addTodo, setAddToDo] = useState({})
   const [todos, setTodos] = useState([])
   const [change, setChange] = useState(false)
 
   useEffect(() => {
-    console.log('hi',todos)
   }, [todos])
 
-  function handleChange (e) {
+  function handleChange(e) {
     setAddToDo({ ...addTodo, [e.target.name]: e.target.value })
   }
 
-  function create (e) {
+  function create(e) {
     e.preventDefault()
     setTodos([...todos, addTodo])
   }
 
+  function todoupdate(id, info) {
+    console.log(info)
+    let newTodos = todos;
+    newTodos[id] = info;
+    setTodos(newTodos);
+    setChange(!change)
+  }
+
   function todoDelete(id) {
-    // console.log(id);
     let newtodos = todos;
-    // console.log(newtodos);
-    newtodos.splice(id,1)
-    // console.log(newtodos);
+    newtodos.splice(id, 1)
     setTodos(newtodos);
     setChange(!change)
   }
@@ -35,7 +39,7 @@ function Home () {
         {/* create */}
         <br />
         <div
-          className='w-10/12 bg-green-100 shadow-md
+          className='w-10/12 bg-green-100 shadow-2xl
             py-5 rounded-lg m-auto'
         >
           <div className='flex'>
@@ -50,7 +54,12 @@ function Home () {
                   className='block text-blue-800 text-md font-semibold mb-2'
                   htmlFor='username'
                 >
-                  Username
+                  <div className='flex'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                    </svg>
+                    &nbsp; Username:
+                  </div>
                 </label>
                 <input
                   className='shadow-xl appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
@@ -58,7 +67,7 @@ function Home () {
                   type='text'
                   placeholder='Username'
                   onChange={e => handleChange(e)}
-                    required
+                  required
                 />
               </div>
               <div className='pl-0 sm:pl-5 flex-1'>
@@ -66,10 +75,18 @@ function Home () {
                   className='block text-blue-800 text-md font-semibold mb-2 pt-5 sm:pt-0'
                   htmlFor='username'
                 >
-                  Email
+                  <div className='flex'>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                    </svg>
+                    &nbsp; Email:
+                  </div>
                 </label>
                 <input
-                  className='shadow-xl appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
+                  className='shadow-xl appearance-none border rounded-xl w-full py-2 px-3
+                  text-gray-700 leading-tight bg-white focus:shadow-outline'
                   name='email'
                   type='email'
                   placeholder='xyz@abc.com'
@@ -83,7 +100,14 @@ function Home () {
                 className='block text-blue-800 text-md font-semibold mb-2'
                 htmlFor='username'
               >
-                To-Do:
+                <div className='flex'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
+                    <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                  </svg>
+                  &nbsp; To-Do:
+                </div>
+
               </label>
               <textarea
                 className='w-full h-48 md:h-24 px-3 py-2 text-base text-gray-700 border rounded-xl focus:shadow-outline'
@@ -104,17 +128,24 @@ function Home () {
         </div>
         <br />
         <div
-          className='w-10/12 bg-green-100 shadow-md
+          className='w-10/12 bg-green-100 shadow-2xl pb-10
             py-5 rounded-lg m-auto flex flex-col lg:flex-row flex-wrap px-10'
         >
-          {todos ? (
-            console.log(todos)
-            ) : (-1)}
           {todos && todos.map((todo, idx) => (
-            <Cards key={idx} id={idx} name={todo.name} email={todo.email} todo={todo.todo} todoDelete={todoDelete}/>
+            <Cards key={idx} id={idx} name={todo.name} email={todo.email} todo={todo.todo} todoDelete={todoDelete} todoUpdate={todoupdate} />
           ))}
+          {todos.length < 1 &&
+            <div className='flex m-auto text-xl text-red-500 font-semibold'>
+              No To-Do Available, Chilll &nbsp; &nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clipRule="evenodd" />
+              </svg>
+            </div>
+          }
         </div>
+        <br />
       </div>
+      
     </div>
   )
 }
